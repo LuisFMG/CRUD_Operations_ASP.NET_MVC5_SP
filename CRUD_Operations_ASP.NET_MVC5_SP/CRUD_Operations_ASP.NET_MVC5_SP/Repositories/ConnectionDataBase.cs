e7df434dfb6142abb41a55d279a5f3cde10b2a28
Luis F. Molina <LuisMG.31@hotmail.com>
2016-02-06 21:00:50 -0700

    Configuración de conexión a base de datos, agregada

---
+++ CRUD_Operations_ASP.NET_MVC5_SP/CRUD_Operations_ASP.NET_MVC5_SP/Repositories/ConnectionDataBase.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CRUD_Operations.Repositories
+{
+    public class ConnectionDataBase
+    {
+        private static string ConnectionString;
+
+        protected SqlConnection ConnectionSQL;
+        protected SqlCommand CmdSQL;
+        protected SqlDataReader ReaderSQL;
+        protected SqlDataAdapter AdapterSQL;
+        protected DataTable TablesSQL;
+
+        public static string ConnectionDB
+        {
+            get
+            {
+                if (ConnectionString == null || ConnectionString == string.Empty)
+                {
+                    ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionDB"].ConnectionString;
+                }
+
+            return ConnectionString;
+            }
+        }
+    }
+}
